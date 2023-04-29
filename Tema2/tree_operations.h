768c39b7fd9505ea1da6947e6afcdd7e7957041a
DariusIuga <98218119+DariusIuga@users.noreply.github.com>
2023-04-29 18:39:37 +0300

    Update tree_operations.h

--- Tema2/tree_operations.h
+++ Tema2/tree_operations.h
@@ -19,7 +19,7 @@ struct tree_node
     unsigned char red;
     unsigned char green;
     unsigned char blue;
-    unsigned int depth;
+    unsigned char depth;
     point corner_top_left;
     point corner_bottom_right;
     tree_node *top_left;
@@ -27,6 +27,13 @@ struct tree_node
     tree_node *bottom_right;
     tree_node *bottom_left;
 };
+
+typedef struct output_values
+{
+    unsigned int depth;
+    unsigned int nr_final_blocks;
+    unsigned int biggest_side;
+} output_values;
 
 void init_tree(tree_node **root, unsigned int width, unsigned int height)
 {
@@ -44,56 +51,59 @@ void init_tree(tree_node **root, unsigned int width, unsigned int height)
     (*root)->bottom_left = NULL;
 }
 
+void init_leaf(tree_node **leaf, tree_node *parent)
+{
+    *leaf = (tree_node *)malloc(sizeof(tree_node));
+    
+    (*leaf)->type = 1;
+    (*leaf)->depth = (parent->depth) + 1;
+    (*leaf)->top_left = NULL;
+    (*leaf)->top_right = NULL;
+    (*leaf)->bottom_right = NULL;
+    (*leaf)->bottom_left = NULL;
+}
+
 void generate_subtrees(tree_node *parent)
 {
     // North-West si South-East
-    point NW, SE;
+    point NW, SE, mid;
     NW.x = parent->corner_top_left.x;
     NW.y = parent->corner_top_left.y;
     SE.x = parent->corner_bottom_right.x;
     SE.y = parent->corner_bottom_right.y;
+    mid.x = (NW.x + SE.x) / 2;
+    mid.y = (NW.y + SE.y) / 2;
+
+    // Este alocata memorie pentru subarbori
+    init_leaf(&parent->top_left,parent);
+    init_leaf(&parent->top_right,parent);
+    init_leaf(&parent->bottom_right,parent);
+    init_leaf(&parent->bottom_left,parent);
 
     // Nodul parinte devine unul de tip inner
     parent->type = 0;
-    // Este alocata memorie pentru subarbori
-    parent->top_left = (tree_node *)malloc(sizeof(tree_node));
-    parent->top_right = (tree_node *)malloc(sizeof(tree_node));
-    parent->bottom_right = (tree_node *)malloc(sizeof(tree_node));
-    parent->bottom_left = (tree_node *)malloc(sizeof(tree_node));
 
     // Subarborele din stanga sus:
     parent->top_left->corner_top_left.x = NW.x;
     parent->top_left->corner_top_left.y = NW.y;
-    parent->top_left->corner_bottom_right.x = (SE.x - NW.x) / 2;
-    parent->top_left->corner_bottom_right.y = (SE.y - NW.y) / 2;
+    parent->top_left->corner_bottom_right.x = mid.x;
+    parent->top_left->corner_bottom_right.y = mid.y;
 
     // Subarborele din dreapta sus:
-    parent->top_right->corner_top_left.x = (SE.x - NW.x) / 2 + 1;
+    parent->top_right->corner_top_left.x = mid.x + 1;
     parent->top_right->corner_top_left.y = NW.y;
     parent->top_right->corner_bottom_right.x = SE.x;
-    parent->top_right->corner_bottom_right.y = (SE.y - NW.y) / 2;
+    parent->top_right->corner_bottom_right.y = mid.y;
 
     // Subarborele din dreapta jos:
-    parent->bottom_right->corner_top_left.x = (SE.x - NW.x) / 2 + 1;
-    parent->bottom_right->corner_top_left.y = (SE.y - NW.y) / 2 + 1;
+    parent->bottom_right->corner_top_left.x = mid.x + 1;
+    parent->bottom_right->corner_top_left.y = mid.y + 1;
     parent->bottom_right->corner_bottom_right.x = SE.x;
     parent->bottom_right->corner_bottom_right.y = SE.y;
 
     // Subarborele din stanga jos:
     parent->bottom_left->corner_top_left.x = NW.x;
-    parent->bottom_left->corner_top_left.y = (SE.y - NW.y) / 2 + 1;
-    parent->bottom_left->corner_bottom_right.x = (SE.x - NW.x) / 2;
+    parent->bottom_left->corner_top_left.y = mid.y + 1;
+    parent->bottom_left->corner_bottom_right.x = mid.x;
     parent->bottom_left->corner_bottom_right.y = SE.y;
-
-    // Nivelul unui nod fiu e cu 1 mai mare fata de cel al parintelui sau
-    parent->top_left->depth = (parent->depth) + 1;
-    parent->top_right->depth = (parent->depth) + 1;
-    parent->bottom_right->depth = (parent->depth) + 1;
-    parent->bottom_left->depth = (parent->depth) + 1;
-
-    // Subarborii noi vor fi frunze
-    parent->top_left->type = 1;
-    parent->top_right->type = 1;
-    parent->bottom_right->type = 1;
-    parent->bottom_left->type = 1;
 }
